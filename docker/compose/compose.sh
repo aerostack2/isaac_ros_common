a1425e5c040201edf9a5f556096928e052f05f78
rdasilva01 <rodrigods2001@gmail.com>
2024-04-25 15:50:24 +0200

    Fix compose script

--- docker/compose/compose.sh
+++ docker/compose/compose.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PLATFORM="$(uname -m)"
+
+echo "Platform $PLATFORM detected"
+
 if [[ $PLATFORM == "aarch64" ]]; then
-    echo "aarch64 architecture detected"
     cat docker-compose-aarch64.txt > docker-compose.yml
 else
-    echo "x86_64 architecture detected"
     cat docker-compose-x86_64.txt > docker-compose.yml
 fi
 
